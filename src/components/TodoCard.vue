ac3cdcd6c9586ea285f10ce2fe0600843f891194
eugenekalib <eugenekalib@yahoo.co.uk>
2024-12-08 20:43:57 +0300

    added reactive todo app button to cards

--- src/components/TodoCard.vue
+++ src/components/TodoCard.vue
@@ -1,13 +1,28 @@
 <template>
-    <div :class="$style.card">
+    <div 
+    :class="$style.card"
+    @mouseover="isHovered = true" 
+    @mouseleave="isHovered = false" 
+    >
         <p> {{ text }} </p>
+        <div :class="$style.buttons">
+            <TodoButton v-show="isHovered" type="change" />
+            <TodoButton v-show="isHovered" type="delete" />
+        </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import TodoButton from '@/components/ui/TodoButton.vue';
+
+const isHovered = ref(false);
+
 defineProps({
-    text: String,
-    required: true,
+    text: {
+        type: String,
+        required: true,
+    }
 })
 </script>
 
@@ -19,5 +34,12 @@ defineProps({
     height: fit-content;
     font-weight: 500;
     font-size: 20px;
+    position: relative;
+}
+.buttons {
+    position: absolute;
+    right: 20px;
+    top: -11px;
+    margin-left: 10px;
 }
 </style>
---
+++ src/components/ui/TodoButton.vue
@@ -0,0 +1,51 @@
+<template>
+    <button :class="[$style.todoButton, buttonClasses]">
+        {{ buttonText }}
+    </button>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useCssModule } from 'vue';
+
+const classesList = useCssModule();
+
+const props = defineProps({
+    type: {
+        type: String,
+        required: true,
+    }
+});
+
+const buttonText = computed(() => {
+    const text = {
+        change: 'Изменить',
+        delete: 'Удалить',
+    };
+    return text[props.type];
+});
+const buttonClasses = computed(() => {
+    const buttonClass = {
+        change: classesList.changeButton,
+        delete: classesList.deleteButton,
+    };
+    return buttonClass[props.type] || '';
+});
+</script>
+
+<style module>
+.todoButton {
+    border-radius: 2px;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 16px;
+    text-transform: lowercase;
+    margin-left: 10px;
+}
+.changeButton {
+    background-color: yellow;
+}
+.deleteButton {
+    background-color: red;
+}
+</style>
